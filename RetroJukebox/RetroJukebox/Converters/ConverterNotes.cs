c6e7f903d8e47ace17ace571668cd442839ac0a8
Drew Greenwald <dgreenwald@sparkcognition.com>
2026-03-28 12:02:36 -0500

    Remove duplicates

--- RetroJukebox/RetroJukebox/Converters/ConverterNotes.cs
+++
@@ -1,7 +0,0 @@
-// This file intentionally left as a namespace placeholder.
-// RepeatModeConverter is defined in Converters.cs.
-// Add the following to MainWindow.xaml Window.Resources if not already present:
-//
-//   <conv:RepeatModeConverter x:Key="RepeatConverter"/>
-//
-// and ensure the xmlns:conv alias points to RetroJukebox.Converters
